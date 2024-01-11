1ce81f3d591fe5a6999bbedd97229105c3d1e403
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-01-11 05:21:05 +0000

    week

--- If_else/weekday.py
+++ If_else/weekday.py
@@ -2,19 +2,19 @@ try:
     Day = int(input("Enter the number from 1 to 7: "))
 
     if(Day == 1):
-        print("Monday")
-    elif(Day == 2):
-        print("Tuesday")
-    elif(Day == 3):
-        print("Wednesday")
-    elif(Day == 4):
-        print("Thursday")
-    elif(Day == 5):
-        print("Friday")
-    elif(Day == 6):
-        print("Saturday")
-    elif(Day == 7):
         print("Sunday")
+    elif(Day == 2):
+        print("Monday")
+    elif(Day == 3):
+        print("Tuesday")
+    elif(Day == 4):
+        print("Wednesday")
+    elif(Day == 5):
+        print("Thursday")
+    elif(Day == 6):
+        print("Friday")
+    elif(Day == 7):
+        print("Saturday")
 
 except Exception as e:
     print("Enter the valid input: ", e)
