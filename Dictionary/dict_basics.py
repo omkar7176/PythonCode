d564d9a861a86c21fbf7973c601df6cdcc55ea20
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-01-20 18:07:07 +0000

    dictionary

---
+++ Dictionary/dict_basics.py
@@ -0,0 +1,3 @@
+                    # ** Dictionary in Python 
+# A dictionary in Python is a data structure that stores the value in value:key pairs.
+# Note: The dictionary do not allow duplicates.but it contain same key which represent the updated value.
