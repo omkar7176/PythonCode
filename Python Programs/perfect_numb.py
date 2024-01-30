bd49ea88645351f58a6922db2764895cf64dc11c
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-01-30 17:06:27 +0000

    fie edited

---
+++ Python Programs/perfect_numb.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the number: "))
+result = 0
+
+for i in range(1, num):
+    if (num%2) == 0
+    result = result+i
+
+if result == num:
+    print(num,  "is a perfect number")
+else:
+    print(num,  "is not perfect number"))
