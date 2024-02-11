e1c533ebbb4bccde82b2391358822481f3193857
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-11 10:55:19 +0000

    factorial program

--- Python Programs/factorial.py
+++ Python Programs/factorial.py
@@ -22,3 +22,27 @@ if num > 0:
     for i in range(1, num+1):
         fact = fact*i
 print("The Factorial of given number is: ", fact)
+
+
+
+
+
+
+
+
+
+
+# Solution 2: Using Recursion
+
+def fact(a):
+    
+    if a == 0:
+        return 1
+    else: 
+        return ((a) * fact(a-1))
+
+num = int(input("Enter the number: "))
+
+result = fact(num)
+
+print("The factorial of the given number: ", result)
