6bfe3f8f73160f121520b5038fa435ae3f0893d0
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-03-07 17:16:30 +0000

    Adding method

---
+++ Sets/set_methods.py
@@ -0,0 +1,15 @@
+''' Set_Methods: 
+    1.  add(): Adds a given element to a set.
+    2.  clear(): Removes all elements from the set.
+    3.  pop(): Returns and removes a random element from the set.
+    6.  remove(): Removes the element from the set.
+    4.  union(): Returns a set that has the union of all sets.
+    5.  discard(): Removes the element from the set.
+    7.  update(): Adds elements to the set
+    8.  ()
+    9.  ()
+    10. '''
+
+
+
+''' 1.add() '''
