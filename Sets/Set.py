38c787e7adb92298a0b19468a7389f0985d02fd0
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-03-08 11:38:50 +0000

    Basics of Sets

--- Sets/Set.py
+++ Sets/Set.py
@@ -1,9 +1,10 @@
-#                                    ** Sets in Python
+#                                    ** Sets in Python **
 # A Set in Python programming is an unordered collection data type that is iterable, mutable 
 # and has no duplicate elements. 
 #  Set are represented by { } (values enclosed in curly braces).
 # Sets are unordered
 # Set elements are unque. Duplcate elements are not allowed in a set.
+# What are sets --> ❌ (indexing, ordered, duplicate, mutable) ❌
 
 
 
@@ -12,19 +13,58 @@
 
 
 # * Sets in Python
+x ={}
+print(x)
+print(type(x))
+# OP: {}
+#     <class 'dict'>
+
+
+fruits = {"apple", "orange", "banana", "cherry"}
+print(fruits)
+# OP: {'banana', 'cherry', 'apple', 'apple'}
+print(type(fruits)) # OP: <class 'set'>
+
+
 # 1st way
-x = {"Omkar", 234, True, False, ("Sanket")}
+x = {"Omkar", 234, True, False, "Sanket"}
 print(x)
 # OP: {False, True, 'Sanket', 'Omkar', 234}
 print(type(x))   # OP: <class 'set'>
+
 
 # 2nd way
 y = set()
 print(y)
 print("type of Y: ", type(y))
+# OP: set()
+#     type of Y:  <class 'set'>
+
+
+# Set removes the duplicate values
+a = {'a', 'b', 'c', 'a'}
+print(a)
+# OP: {'b', 'c', 'a'}
 
 
 
+
+
+
+#get the length of set
+a = {"Shyam", 21, 1000.0, True}
+print(len(a))  #OP: 4
+
+
+
+
+
+##the Set() constructor
+set1 = set(("Ram", 21, 22))
+print(set1)
+print(type(set1))
+# OP: {'Ram', 21, 22}
+#     <class 'set'>
 
 
 
@@ -47,3 +87,28 @@ Name = set(["Omkar", "Sanket", True, False, 32343.33, 32323])
 print(Name)
 print(type(Name))   # OP: <class 'set'>
 # OP: {False, True, 32323, 'Omkar', 32343.33, 'Sanket'}
+
+
+
+
+
+
+
+# Access the items in set using loop
+setA = {"Om", "San", "jac", "dog", "cat", "mat"}
+for i in setA:
+    print(i)
+# OP: Om
+#   San
+#   cat
+#   mat
+#   jac
+#   dog
+    
+
+
+
+# check if items present in set or not using "in" operator 
+fruits = {"Apple", "grapes", "cherry"}
+print("Apple" in fruits)
+# OP: True
