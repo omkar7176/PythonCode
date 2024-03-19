e990f7c757d1c31fcbce99d3c3882864580b547d
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-03-19 18:29:34 +0000

    changes done

--- Dictionary/dict.py
+++ Dictionary/dict.py
@@ -1,53 +1,24 @@
-                    # ** Dictionary in Python 
-# A dictionary in Python is a data structure that stores the value in value:key pairs.
-# Note: The dictionary do not allow duplicates.but it contain same key which represent the updated value.
-
-
-Details = {'Name': "Omkar", 'City': "Pune", 'Comp': "Microsoft"}
-info = {1 : "Ram", 2 : "Delhi", 3 : "Amazon"}
-print(Details)
-print(info)
-
-# Creating the dictionary using function --> dict()
-Student = dict(Name= "Omkar", Field= "Computer Science", Age= 22, Comp="Google")
-print(Student)
-
-
-# Accessing the Dictionary items
-
-Pinfo = {'Name': "Shyam", 'City': "Kolkata", 'eligible': True}
-# 1st way:
-print(Pinfo.get('City'))
-# OP: Kolkata
-
-# 2nd Way but without get()
-print(Pinfo['Name'])
-# OP: Shyam
+                            #   ** Dictionary in Python 
+"""
+# 1. A dictionary in Python is a data structure that stores the value in value:key pairs.
+# 2. Note: The dictionary do not allow duplicates. but it contain same key which represent the updated value.
+# 3. What are the dictionary --> ❌(indexing, duplicate) and  ✔(Ordered, Mutable)
+# 4. "Values" in a dictionary can be of any data type and can be duplicated, whereas "keys" can’t be repeated and must be immutable. 
+# 5. Note - Dictionary keys are case sensitive, the same name but different cases of Key will be treated distinctly. """
 
 
 
 
+''' Dictionary Syntax:
+# var = {key1 : value1, key2 : value2, ......}
+'''
 
-# Accessing the multiple values
-print(Pinfo.values())
-# OP: dict_values(['Shyam', 'Kolkata', True])
-
-
-
-
-
-# Note: if the key is not presented in the dict & we print it the OP is None.
-print(Pinfo.get('Color'))
-# OP: None
-
-
-
-
-
-
-# Accessing the keys:
-print(Pinfo.keys())
-# OP: dict_keys(['Name', 'City', 'eligible'])
+# Create the empty dictionary
+empty_var = {}
+print("Empty_Dictionary: ", empty_var)
+print(type(empty_var))
+# OP: {}
+#    <class 'dict'>
 
 
 
@@ -57,50 +28,93 @@ print(Pinfo.keys())
 
 
 
-# Accesing the key:values pair in single line.
-print(Pinfo.items())
-# OP: dict_items([('Name', 'Shyam'), ('City', 'Kolkata'), ('eligible', True)])
+# Create the dict with different data types of elements.
+student = {
+    'Name': "Shyam",
+    'Country': "India", 
+    'Age': 21, 
+    'Working': True, 
+    'CGPA': 8.85
+    }
+print("Elements in dictionary: ",student)
+print(type(student))
+# OP: Elements in dictionary:  {'Name': 'Shyam', 'Country': 'India', 'Age': 21, 'Working': True, 'CGPA': 8.85}
+#     <class 'dict'>
+
+
+
+
+
+# Different ways to create the Dictionary
+
+# Example 1
+# A dictionary can also be created by the built-in function dict().
+var1 = dict({
+    'Name': 'Ram', 
+    'Age': 22
+    })
+print(var1)
+print(type(var1))
+# OP: {'Name': 'Ram', 'Age': 22}
+#     <class 'dict'>
+
+
+# Example 2
+var2 = dict([
+    (1, 'Omkar'), 
+    (2, 'Ram'), 
+    (3, 'Shyam')
+    ])
+print(var2)
+print(type(var2))
+# OP: {1: 'Omkar', 2: 'Ram', 3: 'Shyam'}
+#     <class 'dict'>
 
 
 
 
 
 
-# Add items in Dictionary using update()
-# 1st way:
-emp = {'Name': "Shyam", 'Age' : 22, 'Company': 'Amazon', 'Year': 2024}
-emp.update(Study="DSA")
-print(emp)
-#OP: {'Name': 'Shyam', 'Comp': 'Amazon', 'Age': 22, 'Year': 2024, 'Study': 'DSA'}
 
 
 
-# 2nd way:
-sample = {'Name': "Ram", 'Comp': "Google", 'Age' : 22, 'Year': 2023}
+# Add the elements in empty dictionary.
+
+# Example 1:
+a = {}
+print("Empty Dictionary: ",a)
+a[1] = "Hello, "
+a[0] = "Good Morning"
+a[2] = "Everyone"
+a[0] = "Good Afternoon" #Always print the updated value
+a['values'] = 5678
+print("After the adding elements: ", a)
+# OP: Empty Dictionary:  {}
+#     After the adding elements:  {1: 'Hello, ', 0: 'Good Afternoon', 2: 'Everyone', 'values': 5678}
+
+
+
+
+
+# Example 2:
+sample = {
+    'Name': "Ram", 
+    'Comp': "Google", 
+    'Age' : 22, 
+    'Year': 2023
+    }
 sample["Domain"] = "SDE"
 print(sample)
-
 # OP: {'Name': 'Ram', 'Comp': 'Google', 'Age': 22, 'Year': 2023, 'Domain': 'SDE'}
 
 
 
 
 
-# Change the values in the dictionary.
 
-country_info = {'IN': 'India', "CA": "England", "US": 'USA'}
-country_info["CA"] = "Canada"
-print(country_info)
-
-
-
-
-
-
-
-# Adding elements to dictionary using different ways like using array indexing.
+# Example 3:
 Language = {}
-# print(Language)
+print(Language)
 
 Language[1]= "Python"
 Language[2]= "Java"
@@ -109,7 +123,6 @@ Language[4]= "Typescript"
 print(Language)   # {1: 'Python', 2: 'Java', 3: 'Javascript', 4: 'Typescript'}
 Language[4]= "C++"
 print(Language) # {1: 'Python', 2: 'Java', 3: 'Javascript', 4: 'C++'}
-
 #OP: That's the main difference
 
 
@@ -117,54 +130,74 @@ print(Language) # {1: 'Python', 2: 'Java', 3: 'Javascript', 4: 'C++'}
 
 
 
+# Accesing the elements from dictionary
 
-# Use Dictionary with the mixed keys and accesing the elements.
-Dict  = {'Name': 'Rajesh', 'Numb': [1,2,3,4,5]}
-
-print(Dict['Numb']) #--> OP: 'Numb': [1,2,3,4,5,6,7,8,9,0]
-
-print(Dict)  #--> OP: {'Name': 'Rajesh', 'Numb': [1,2,3,4,5,6,7,8,9,0]}
-
-
-
-
-
-
-# Different ways to create the dictionary in Python
-
-info = {}
-
-# using the dict() function
-info = dict({1: 'Shreyash', 2: 22, 3: 'CS', 4: 'IIT Madras'})
-print(info)
-# OP: {1: 'Jayesh', 2: 22, 3: 'CS', 4: 'IIT Madras'}
+# Example 1:
+candidate = {
+    "Name" : "Ram", 
+    'Country': "India", 
+    'Age': 21, 
+    'Working': True, 
+    'CGPA': 8.85
+    }
+print(candidate['Country'])
+# OP: India
 
 
 
-
-
-
-# remove the element from the dict using  pop().
-Student = {1 : "Omkar", 2: "Sanket", 3:"Kunal", 4: "Shreyash", 5: "John"}
-Student.pop(5)
-print(Student)
-#OP: {1: 'Omkar', 2: 'Sanket', 3: 'Kunal', 4: 'Shreyash'}
-candidate = Student.pop(5)
-print(candidate)
-#OP: John
+# Example 2:
+color = {
+    1:'White', 
+    2:'Red', 
+    3:'Green', 
+    4:'blue', 
+    5: 'Yellow'
+    }
+print(color)
+print(type(color))
+print(color[5])
+# OP: {1: 'White', 2: 'Red', 3: 'Green', 4: 'blue', 5: 'Yellow'}
+#     <class 'dict'>
+#     Yellow
 
 
 
 
 
 
-# Deleting Elements using ‘del’ Keyword
-Student = {1 : "Omkar", 2: "Sanket", 3:"Kunal", 4: "Shreyash", 5: "John"}
-del(Student[5])
-print(Student)
-#OP: {1: 'Omkar', 2: 'Sanket', 3: 'Kunal', 4: 'Shreyash'}
+
+
+# Accessing an Element of a Nested Dictionary
+
+alphabet = {1:{'IND': "India"}, 
+            4:{'UK': "United Kingdom"},
+            2:{'Ger': 'Germany'},
+            3:{'USA': 'United State of America'}
+            }
+print(alphabet[4]['UK'])
+print(alphabet[1])
+# OP: United Kingdom
+#    {'IND': 'India'}
 
 
 
 
-#          **___Completed___**
+
+
+
+# Change the values in the dictionary.
+
+country_info = {
+    'IN': "India", 
+    'CA': "England", 
+    'AUS': "Australia"
+    }
+country_info["CA"] = "Canada"
+print(country_info)
+# OP: {'IN': 'India', 'CA': 'Canada', 'AUS': 'Australia'}
+
+
+
+
+'''___Completed___'''
+# Recently Done
